a74672d717554781a1ff9aabc9c008198b7d38c8
Brett Dorrans <hello@lapidist.net>
2025-07-05 14:44:11 +0100

    fix: include mantine styles

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
