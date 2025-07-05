b610ac8a93b352e1414e77f43ed9664aa53ba8e2
Brett Dorrans <hello@lapidist.net>
2025-07-05 10:06:39 +0100

    Add audio device selection

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
 
 use bitmxr::{
     audio_engine::AudioEngine, get_audio_stats as get_audio_stats_impl,
-    list_audio_devices as list_audio_devices_impl,
+    list_audio_devices as list_audio_devices_impl, set_audio_device as set_audio_device_impl,
 };
 
 #[tauri::command]
@@ -15,12 +15,19 @@ fn list_audio_devices() -> Vec<String> {
     list_audio_devices_impl()
 }
 
+#[tauri::command]
+fn set_audio_device(id: String) {
+    set_audio_device_impl(&id);
+}
+
 fn main() {
+    tracing_subscriber::fmt::init();
     tauri::Builder::default()
         .manage(AudioEngine::default())
         .invoke_handler(tauri::generate_handler![
             get_audio_stats,
-            list_audio_devices
+            list_audio_devices,
+            set_audio_device
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
